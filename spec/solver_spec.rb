7a36b82a4a297aa024b0310ad0467f404552a0ec
abdessittir harkaty <abdessattarharkaty@gmail.com>
2023-02-10 10:18:09 +0100

    Add tests for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -16,4 +16,22 @@ describe Solver do
       expect { @solver.factorial(-1) }.to raise_error(TypeError)
     end
   end
+
+  context 'fizzbuzz method' do
+    it 'should return "fizz" for number divisible by 3' do
+      expect(@solver.fizzbuzz(3)).to eq('fizz')
+    end
+
+    it 'should return "buzz" for number divisible by 5' do
+      expect(@solver.fizzbuzz(5)).to eq('buzz')
+    end
+
+    it 'should return "fizzbuzz" for number divisible by 3 and 5' do
+      expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'should return a string with the same number (7 => "7")' do
+      expect(@solver.fizzbuzz(7)).to eq('7')
+    end
+  end
 end
